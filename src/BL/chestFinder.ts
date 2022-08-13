f5fdbd99b743a34ed5a27c1af0f03eb1d413c86c
eyal-hl <eyalhermonilahat@gmail.com>
2022-08-13 18:18:41 +0300

    now removing all special chars

--- src/BL/chestFinder.ts
+++ src/BL/chestFinder.ts
@@ -3,7 +3,7 @@ import { api } from "../DA/chestFinder/chestFinderApi";
 
 export const urlsOfChampsWithChests = async(region:string, summoner_name:string):Promise<string[]> => {
     const chestInfo:ChestInfo = await api.getChestsInfo(region.toUpperCase(),summoner_name)
-    const champsWithChest = chestInfo.has_chest.map(champ=>champ.name.replace(`'`,'').split(' ').map(word=>word[0].toUpperCase()+word.slice(1).toLowerCase()).join(''))
+    const champsWithChest = chestInfo.has_chest.map(champ=>champ.name.replace(/[^a-zA-Z0-9 ]/g, '').split(' ').map(word=>word[0].toUpperCase()+word.slice(1).toLowerCase()).join(''))
     champsWithChest.forEach((champ,index)=> {
         if(champ=="Wukong") champsWithChest[index] = "MonkeyKing"
         if(champ=="Nunu&Willump") champsWithChest[index] = "Nunu"
