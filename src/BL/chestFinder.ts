3ad4037ba9c8ebf8de80a8f1c5278cdb2e466107
eyal-hl <eyalhermonilahat@gmail.com>
2022-08-13 18:32:07 +0300

    apperntly this exists

--- src/BL/chestFinder.ts
+++ src/BL/chestFinder.ts
@@ -3,13 +3,7 @@ import { api } from "../DA/chestFinder/chestFinderApi";
 
 export const urlsOfChampsWithChests = async(region:string, summoner_name:string):Promise<string[]> => {
     const chestInfo:ChestInfo = await api.getChestsInfo(region.toUpperCase(),summoner_name)
-    const champsWithChest = chestInfo.has_chest.map(champ=>champ.name.replace(/[^a-zA-Z0-9 ]/g, '').split(' ').map(word=>word[0].toUpperCase()+word.slice(1).toLowerCase()).join(''))
-    champsWithChest.forEach((champ,index)=> {
-        if(champ=="Wukong") champsWithChest[index] = "MonkeyKing"
-        if(champ=="Nunu&Willump") champsWithChest[index] = "Nunu"
-        if(champ=="RenataGlasc") champsWithChest[index] = "Renata"
-    })
-    const champsWithChestURL = champsWithChest.map(champ=>`https://ddragon.leagueoflegends.com/cdn/12.15.1/img/champion/${champ}.png`)
+    const champsWithChestURL = chestInfo.has_chest.map(champ=>`https://ddragon.leagueoflegends.com/cdn/12.15.1/img/champion/${champ.id}.png`)
 
     return champsWithChestURL
 }
